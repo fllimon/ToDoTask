ef17807e9f1f6aa9d1a20e4e475e39f6abadbea5
Artur Sharaenko <fallout2408@gmail.com>
2021-09-15 11:59:37 +0300

    Add class with CRUD to DB

---
+++ ToDoApi/ToDoCrudDatabase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApi
+{
+    public class ToDoCrudDatabase : IToDoCrud
+    {
+        public Task AddAsync(ToDo item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ToDo> FindAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ToDo> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ToDo Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ToDo item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
