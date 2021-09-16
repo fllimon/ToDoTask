38eed9dd0cfb1cebe2d97fcda316e7e7ff3042f7
Artur Sharaenko <fallout2408@gmail.com>
2021-09-16 18:48:16 +0300

    add DbContext

--- ToDoApi/Startup.cs
+++ ToDoApi/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDoApi
 {
@@ -26,6 +27,7 @@ namespace ToDoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ToDoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped<IToDoCrud, ToDoCrudJson>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
---
+++ ToDoApi/ToDoContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApi
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> option) 
+            : base(option)
+        {
+
+        }
+
+        public DbSet<ToDo> ToDo { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ToDo>().ToTable("ToDo");
+        }
+    }
+}
