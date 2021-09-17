af097a5a6595878d4f30405d4ced3a36a66a82ed
Artur Sharaenko <fallout2408@gmail.com>
2021-09-17 12:01:00 +0300

    add atribute on property

--- ToDoApi/ToDo.cs
+++ ToDoApi/ToDo.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +10,20 @@ namespace ToDoApi
 {
     public class ToDo
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long? Id { get; set; }
+
+        [Column("KeyToDo")]
         public string Key { get; set; }
 
-        public string Name { get; set; }
+        [Column("DescriptionToDo")]
+        public string Description { get; set; }
 
+        [Column("DateTimeToDo")]
         public DateTime Date { get; set; }
 
-        public bool IsComplete { get; set; } = false;
+        [Column("IsComplete")]
+        public bool IsComplete { get; set; } = false;    //ToDo: tinyint to bool ?? 
     }
 }
