018caa65b5b0aed1172a8f1fbe17e90ce20e5de7
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-09-28 10:30:17 +0300

    Add EF migrations

---
+++ ToDoApi/Migrations/20210928072147_ToDoApi.ToDoContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoApi.Migrations
+{
+    public partial class ToDoApiToDoContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDo",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Key = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsComplete = table.Column<byte>(type: "tinyint", nullable: false),
+                    IsDeleted = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDo");
+        }
+    }
+}
