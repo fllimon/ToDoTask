ff93f4d031a6441a1e9449e9ec914e8c34b2d4d2
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-10-01 13:20:15 +0300

    Remove connectionString

--- ToDoApi/Startup.cs
+++ ToDoApi/Startup.cs
@@ -27,8 +27,6 @@ namespace ToDoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ToDoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            
             if (ConfigurationValue(Configuration))
             {
                 services.AddScoped<IToDoCrud, ToDoCrudDatabase>();
