0570cdce7329ad5018e77ee82376eae3e4d911ec
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-10-01 13:48:36 +0300

    Fix CRUD and Move repos to folder

---
+++ ToDoApi/Repositories/ToDoCrudDatabase.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApi
+{
+    public class ToDoCrudDatabase : IToDoCrud
+    {
+        private readonly string _connectionString;
+        private ToDoContext _db;
+
+        public ToDoCrudDatabase(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<bool> AddAsync(ToDo item)
+        {
+            bool isAdd = false;
+            DbContextOptions<ToDoContext> option = GetOption();
+
+            using (_db = new ToDoContext(option))
+            {
+                if (item == null)
+                {
+                    return isAdd;
+                }
+
+                _db.ToDo.Add(item);
+                await _db.SaveChangesAsync();
+
+                return isAdd = true;
+            }
+        }
+
+        public ToDo GetToDoById(long id)
+        {
+            DbContextOptions<ToDoContext> option = GetOption();
+
+            using (_db = new ToDoContext(option)) 
+            {
+                ToDo data = FindToDoById(id);
+
+                return data;
+            }
+        }
+
+        public async Task<IEnumerable<ToDo>> GetAllAsync()
+        {
+            DbContextOptions<ToDoContext> option = GetOption();
+
+            using (_db = new ToDoContext(option))
+            {
+
+                var data = _db.ToDo.Where(a => a.IsDeleted != 1);
+
+                return await data.ToListAsync();
+            }
+        }
+
+        public async Task<bool> Remove(long id)
+        {
+            bool isDeleted = false;
+            DbContextOptions<ToDoContext> option = GetOption();
+
+            using (_db = new ToDoContext(option))
+            {
+
+                ToDo data = FindToDoById(id);
+
+                if (data == null)
+                {
+                    return isDeleted;
+                }
+
+                data.IsDeleted = 1;
+
+                _db.ToDo.Update(data);
+                await _db.SaveChangesAsync();
+
+                return isDeleted = true;
+            }
+        }
+
+        public async Task<bool> Update(ToDo item)
+        {
+            bool isUpdated = false;
+            DbContextOptions<ToDoContext> option = GetOption();
+
+            using (_db = new ToDoContext(option))
+            {
+                if (item == null)
+                {
+                    return isUpdated;
+                }
+
+                ToDo obj = FindToDoById(item.Id);
+
+                if (obj == null)
+                {
+                    return isUpdated;
+                }
+
+                obj.Description = item.Description;
+                obj.Date = item.Date;
+                obj.IsComplete = item.IsComplete;
+                obj.IsDeleted = item.IsDeleted;
+
+                _db.ToDo.Update(obj);
+                await _db.SaveChangesAsync();
+
+                return isUpdated = true;
+            }
+        }
+
+        private ToDo FindToDoById(long id)
+        {
+            return _db.ToDo.Where(x => x.Id == id && x.IsDeleted != 1).FirstOrDefault();
+        }
+
+        private DbContextOptions<ToDoContext> GetOption()
+        {
+            DbContextOptionsBuilder<ToDoContext> optionBuilder = new DbContextOptionsBuilder<ToDoContext>();
+            DbContextOptions<ToDoContext> option = optionBuilder.UseSqlServer(_connectionString).Options;
+
+            return option;
+        }
+    }
+}
---
+++ ToDoApi/Repositories/ToDoCrudJson.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ToDoApi
+{
+    class ToDoCrudJson : IToDoCrud
+    {
+        public const string FILE_NAME = "todo.json";
+        private List<ToDo> _lists;
+
+        public async Task<bool> AddAsync(ToDo item)
+        {
+            return await Task.Run(() => Add(item));
+        }
+
+        public ToDo GetToDoById(long id)
+        {
+             //return await Task.Run(() => GetDataOnKey(key));
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<ToDo>> GetAllAsync()
+        {
+            StreamReader reader = new StreamReader(FILE_NAME);
+
+            ToDo data = await JsonSerializer.DeserializeAsync<ToDo>(reader.BaseStream);   
+
+            _lists = new List<ToDo>();
+
+            _lists.Add(data);
+
+            return _lists;
+        }
+
+        public async Task<bool> Remove(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Update(ToDo item)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool Add(ToDo item)
+        {
+            bool isAdd = false;
+
+            if (item == null)
+            {
+                return isAdd;
+            }
+
+            string jsonString = JsonSerializer.Serialize(item);
+            File.WriteAllText(FILE_NAME, jsonString);
+
+            return isAdd = true;
+        }
+
+        private ToDo GetDataOnKey(string key)
+        {
+            ToDo data = JsonSerializer.Deserialize<ToDo>(File.ReadAllText(FILE_NAME));
+
+            if (data.Description.Equals(key))
+            {
+                return data;
+            }
+
+            throw new KeyNotFoundException("Data not found");
+        }
+    }
+}
