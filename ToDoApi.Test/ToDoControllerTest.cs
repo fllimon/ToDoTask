4ac3fb164630a2ff6e7a8ff96c53c5b8bf9d5ff3
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-10-08 09:53:55 +0300

    Add simple test for Add, Remove< Update, Get, GetById methods

--- ToDoApi.Test/ToDoControllerTest.cs
+++ ToDoApi.Test/ToDoControllerTest.cs
@@ -10,40 +10,101 @@ namespace ToDoApi.Test
 {
     public class ToDoControllerTest
     {
+        //[Fact]
+        //public async Task ViewDataNotNull()
+        //{
+        //    var data = new ToDo(5, "saf", DateTime.Now, 0, 0);
+
+        //    //var mockToDoCrud = new Mock<IToDoCrud>();
+
+        //    //mockToDoCrud.Setup(x => x.GetAllAsync()).ReturnsAsync(data);
+
+        //    //ToDoController controller = new ToDoController(mockToDoCrud.Object);
+
+        //    //var taskResult = controller.Get();
+        //    //Task.WaitAll(taskResult);
+        //    //var resultFromTask = taskResult.Result;    // Выполняется в await под капотом 
+
+        //    //var result = await controller.Get() as OkObjectResult;
+
+        //    //Assert.NotNull(result);
+        //    //Assert.Equal(200, result.StatusCode);
+        //}
+
         [Fact]
-        public async Task ViewDataNotNull()
+        public async Task AddData()
         {
-            var data = new ToDo(5, "saf", DateTime.Now, 0, 0);
+            var data = new ToDo(0, "fjrm", DateTime.Now, 0, 0);
 
-            //var mockToDoCrud = new Mock<IToDoCrud>();
-
-            //mockToDoCrud.Setup(x => x.GetAllAsync()).ReturnsAsync(data);
-
-            //ToDoController controller = new ToDoController(mockToDoCrud.Object);
-
-            //var taskResult = controller.Get();
-            //Task.WaitAll(taskResult);
-            //var resultFromTask = taskResult.Result;    // Выполняется в await под капотом 
-
-            //var result = await controller.Get() as OkObjectResult;
-
-            //Assert.NotNull(result);
-            //Assert.Equal(200, result.StatusCode);
-        }
-
-        [Fact]
-        public void AddData()
-        {
             var mockToDoCrud = new Mock<IToDoCrud>();
+
+            mockToDoCrud.Setup(x => x.AddAsync(data)).ReturnsAsync(true);
 
             ToDoController controller = new ToDoController(mockToDoCrud.Object);
 
+            var result = await controller.Post(data);
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteData()
+        {
+            var data = new ToDo(5, "fjrm", DateTime.Now, 0, 0);
+
+            var mockToDoCrud = new Mock<IToDoCrud>();
+            mockToDoCrud.Setup(x => x.Remove(data.Id)).ReturnsAsync(true);
+
+            ToDoController controller = new ToDoController(mockToDoCrud.Object);
+
+            var result = await controller.Delete(5);
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task UpdateData()
+        {
+            var data = new ToDo(5, "fjrm", DateTime.Now, 0, 0);
+
+            var mockToDoCrud = new Mock<IToDoCrud>();
+            mockToDoCrud.Setup(x => x.Update(data)).ReturnsAsync(true);
+
+            ToDoController controller = new ToDoController(mockToDoCrud.Object);
+
+            var result = await controller.Put(data);
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task GetData()
+        {
             var data = new ToDo(4, "fjrm", DateTime.Now, 0, 0);
 
-            var result = controller.Post(data);
+            var mockToDoCrud = new Mock<IToDoCrud>();
+            mockToDoCrud.Setup(x => x.GetAllAsync()).ReturnsAsync(new List<ToDo>());
 
-            Assert.NotNull(result);
+            ToDoController controller = new ToDoController(mockToDoCrud.Object);
 
+            var result = await controller.Get();
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task GetById()
+        {
+            var data = new ToDo(4, "fjrm", DateTime.Now, 0, 0);
+
+            var mockToDoCrud = new Mock<IToDoCrud>();
+            mockToDoCrud.Setup(x => x.GetToDoById(data.Id)).ReturnsAsync(data);
+
+            ToDoController controller = new ToDoController(mockToDoCrud.Object);
+
+            var result = await controller.GetById(data.Id);
+
+            Assert.IsType<OkObjectResult>(result);
         }
     }
 }
