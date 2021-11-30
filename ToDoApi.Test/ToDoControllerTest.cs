744e5e1c639770d1a87a904fbf298525365e9ab1
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-11-30 15:06:03 +0200

    Updated test on fluent assertions

--- ToDoApi.Test/ToDoControllerTest.cs
+++ ToDoApi.Test/ToDoControllerTest.cs
@@ -8,6 +8,9 @@ using System;
 using Domain.Interfaces;
 using ToDoApi.Models;
 using FluentAssertions;
+using ToDo.EFDatabase.Context;
+using Microsoft.EntityFrameworkCore;
+using FluentValidation.Results;
 
 namespace ToDoApi.Test
 {
@@ -16,7 +19,7 @@ namespace ToDoApi.Test
         private ToDoController _ctrl;
         private Mock<IToDoCrud> _mockToDoCrud;
         private Mock<IToDoFactory> _mockFactory;
-
+        
         public ToDoControllerTest()
         {
             _mockToDoCrud = new Mock<IToDoCrud>();
@@ -33,13 +36,73 @@ namespace ToDoApi.Test
             var returnResult = new ToDoTest();
 
             _mockFactory.Setup(x => x.GetToDo(It.IsAny<long>(), It.IsAny<string>(), It.IsAny<DateTime>(), It.IsAny<byte>(), It.IsAny<byte>())).Returns(returnResult);
-            _mockToDoCrud.Setup(x => x.AddAsync(It.IsAny<IToDo>())).ReturnsAsync(true);
+            _mockToDoCrud.Setup(x => x.AddAsync(It.IsAny<IToDo>())).ReturnsAsync(true).Verifiable();
 
             //Act
             var result = await _ctrl.Post(data);
 
             // Assertion
-            result.Should().BeOfType<OkObjectResult>();  
+            result.Should().BeOfType<OkObjectResult>();
+            _mockToDoCrud.VerifyAll();
+        }
+
+        [Fact]
+        public async Task AddDataBadResult()
+        {
+            //Arrange
+            var data = new PostToDo { Date = DateTime.MaxValue, Description = "aaf" };
+            var returnResult = new ToDoTest();
+
+            _mockFactory.Setup(x => x.GetToDo(It.IsAny<long>(), It.IsAny<string>(), It.IsAny<DateTime>(), It.IsAny<byte>(), It.IsAny<byte>())).Returns(returnResult);
+            _mockToDoCrud.Setup(x => x.AddAsync(It.IsAny<IToDo>())).ReturnsAsync(false).Verifiable();
+
+            //Act
+            var result = await _ctrl.Post(data);
+
+            // Assertion
+            result.Should().BeOfType<BadRequestObjectResult>();
+            _mockToDoCrud.VerifyAll();
+        }
+
+        [Fact]
+        public async Task UpdateDataBadResult()
+        {
+            //Arrange
+            var data = new PutToDo { Id = 2, Date = DateTime.MaxValue, Description = "aaf", IsComplete = 0 };
+            var returnResult = new ToDoTest();
+
+            _mockFactory.Setup(x => x.GetToDo(It.IsAny<long>(), It.IsAny<string>(), It.IsAny<DateTime>(), It.IsAny<byte>(), It.IsAny<byte>())).Returns(returnResult);
+            _mockToDoCrud.Setup(x => x.UpdateAsync(It.IsAny<IToDo>())).ReturnsAsync(false).Verifiable();
+            _mockToDoCrud.Setup(x => x.IsIdExist(It.IsAny<long>())).ReturnsAsync(true);
+
+            //Act
+            var result = await _ctrl.Put(data);
+
+            // Assertion
+            result.Should().BeOfType<BadRequestObjectResult>();
+            var res = result as ObjectResult;
+            var value = res.Value as List<ValidationFailure>;
+            value.Should().Contain(x => x.ErrorMessage == "Description must greater then 5");
+            _mockToDoCrud.VerifyAll();
+        }
+
+        [Fact]
+        public async Task UpdateDataOkResult()
+        {
+            //Arrange
+            var data = new PutToDo { Id = 2, Date = DateTime.MaxValue, Description = "aaffdfdsf", IsComplete = 0 };
+            var returnResult = new ToDoTest();
+           
+            _mockFactory.Setup(x => x.GetToDo(It.IsAny<long>(), It.IsAny<string>(), It.IsAny<DateTime>(), It.IsAny<byte>(), It.IsAny<byte>())).Returns(returnResult);
+            _mockToDoCrud.Setup(x => x.UpdateAsync(It.IsAny<IToDo>())).ReturnsAsync(true).Verifiable();
+            _mockToDoCrud.Setup(x => x.IsIdExist(It.IsAny<long>())).ReturnsAsync(true);
+
+            //Act
+            var result = await _ctrl.Put(data);
+
+            // Assertion
+            result.Should().BeOfType<OkObjectResult>();
+            _mockToDoCrud.VerifyAll();  // Проверка на то что вызывались все методы помеченые Verifiable
         }
     }
 }
--- ToDoApi.Test/ToDoTest.cs
+++ ToDoApi.Test/ToDoTest.cs
@@ -9,10 +9,10 @@ namespace ToDoApi.Test
 {
     public class ToDoTest : IToDo
     {
-        public long Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public DateTime Date { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public byte IsComplete { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public byte IsDeleted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public long Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public byte IsComplete { get; set; }
+        public byte IsDeleted { get; set; }
     }
 }
